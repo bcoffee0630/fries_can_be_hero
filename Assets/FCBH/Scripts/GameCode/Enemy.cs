5dd5ece740f5e4556723f2e249e1db116a3a030e
bcoffee0630 <44256232+bcoffee0630@users.noreply.github.com>
2025-05-18 09:58:27 +0800

    feat : added method to kill all enemies.

--- Assets/FCBH/Scripts/GameCode/Enemy.cs
+++ Assets/FCBH/Scripts/GameCode/Enemy.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using PDollarGestureRecognizer;
 using UnityEngine;
 using UnityEngine.UI;
@@ -13,16 +14,22 @@ namespace FCBH
         
         private string _gesture;
 
-        private static event Action OnRunAway;
+        private static List<Enemy> CreatedEnemies = new();
+
+        public static event Action OnRunAway;
         public static event Action OnKill;
 
         #region Unity methods
 
         private void Start()
         {
+            if (!CreatedEnemies.Contains(this))
+                CreatedEnemies.Add(this);
+            
             _gesture = GestureUtility.GetRandomGesture();
             var texture = config.GestureVisualDatabase.GetGestureVisual(_gesture);
             visual.sprite = texture.gestureTexture;
+            
             if (config.AutoKill)
                 Invoke(nameof(RunAway), config.AutoKillDelay); // todo set randomize time
         }
@@ -41,6 +48,18 @@ namespace FCBH
         {
             OnRunAway?.Invoke();
             Destroy(gameObject); // todo pooling, animate, effect
+        }
+
+        public static void KillAllEnemies()
+        {
+            if (CreatedEnemies.Count == 0)
+                return;
+            for (var x = CreatedEnemies.Count - 1; x >= 0; x--)
+            {
+                var createdEnemy = CreatedEnemies[x];
+                Destroy(createdEnemy.gameObject);
+                CreatedEnemies.RemoveAt(x);
+            }
         }
     }
 }
